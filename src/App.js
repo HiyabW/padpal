f1256eb92f8a2bfaa0f13242d69e9e82f04c4972
heyab <hwold002@ucr.edu>
2024-11-12 14:04:23 -0800

    Added route in App.js for when page is not found in an attempt to fix routes not working in deploy

--- src/App.js
+++ src/App.js
@@ -15,18 +15,21 @@ function App() {
 
   return (
     <>
-    {
-      (location.pathname !== "/" && location.pathname !== "/survey") &&
-      <NavBar />
-    }
-    <Routes>
-      <Route index path='/' element={<SignIn />} />
-      <Route index path='/survey' element={<Survey />} />
-      <Route index path='/feed' element={<Feed />} />
-      <Route index path='/chat' element={<Chat />} />
-      <Route index path='/viewProfile' element={<ViewProfile />} />
-      <Route index path='/editProfile' element={<EditProfile />} />
-    </Routes></>
+      {
+        (location.pathname !== "/" && location.pathname !== "/survey") &&
+        <NavBar />
+      }
+      <Routes>
+        <Route index exact path='/' element={<SignIn />} />
+        <Route index exact path='/survey' element={<Survey />} />
+        <Route index exact path='/feed' element={<Feed />} />
+        <Route index exact path='/chat' element={<Chat />} />
+        <Route index exact path='/viewProfile' element={<ViewProfile />} />
+        <Route index exact path='/editProfile' element={<EditProfile />} />
+        <Route path="*">
+          <h2>Page Not Found</h2>
+        </Route>
+      </Routes></>
   );
 }
 
